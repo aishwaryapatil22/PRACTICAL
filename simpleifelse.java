ae49dc6f236b4832077db0491a0c3fe36d3c293c
aishwaryapatil22 <127470379+aishwaryapatil22@users.noreply.github.com>
2023-03-17 12:10:11 +0530

    Add files via upload

---
+++ simpleifelse.java
@@ -0,0 +1,12 @@
+class simpleifelse{
+public static void main(String args[]){
+int x=5;
+int y=3;
+if (x+y <7) {//true
+System.out.println("x + y is less than 10");
+}
+else { //false
+System.out.println("a+b is greater than 10");
+}
+}
+}
