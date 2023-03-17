6cf3a0384e73963e788568d2753f51a40c2141a6
aishwaryapatil22 <127470379+aishwaryapatil22@users.noreply.github.com>
2023-03-17 12:22:23 +0530

    Add files via upload

---
+++ ifelseladder.java
@@ -0,0 +1,17 @@
+class ifelseladder{
+public static void main(String[] args){
+String name="pallavi";
+if (name =="Meena"){
+System.out.println("name is meena");
+}
+else if (name == "Neha"){
+System.out.println("name is neha");
+}
+else if (name == "Amit"){
+System.out.println("name is amit");
+}
+else {
+System.out.println("name ");
+}
+}
+}
