ae3f7dd206e914b9678b4fe30b5d1e082a8c85ce
aishwaryapatil22 <127470379+aishwaryapatil22@users.noreply.github.com>
2023-04-18 12:45:39 +0530

    Add files via upload

---
+++ DuplicateArray.java
@@ -0,0 +1,22 @@
+package Array;
+
+import java.util.Arrays;
+
+public class DuplicateArray {
+	
+	    public static void main(String[] args) {
+	    	
+	    	int[] arr = {2, 3, 4, 5, 2, 4};
+	    	// Find duplicates in the array
+	        for (int i=0; i<arr.length-1; i++) {
+	            for (int j = i + 1; j < arr.length; j++) {
+	                if (arr[i] == arr[j]) {
+	                    System.out.println("The Duplicate Array is: " + arr[i]);
+
+
+	    }
+	}
+}
+	        }
+	    }
+	    
